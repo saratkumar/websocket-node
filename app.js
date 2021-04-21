428fb470ad7fb5b00690524320d95daac6029d3c
sarath <sarath.kumar2@tatacommunications.com>
2021-04-21 13:40:46 +0530

    disconnected mode tested

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express();
+const router = express.Router();
+const cors = require('cors');
+const http = require('http');
+const fs = require("fs");
+const helper = require("./constants/common")
+
+const process = require('process');
+
+app.use(cors());
+app.use('/api', router);
+router.get('/get-stats', (req, res) => {
+  // Allocating process module
+  const process = require('process');
+  res.json({ cpuUsage: process.cpuUsage(), currentTime: new Date(new Date().toUTCString())});
+  
+});
+
+
+const server = http.Server(app);
+
+// Read users.json file
+
+// const socketIO = require('socket.io');
+const io = require("socket.io")(server, {
+  cors: {
+    origin: helper.DEV_URL,
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+  
+  io.emit(helper.EVENTLIST.CONNECT_STATUS, true);
+  socket.on(helper.EVENTLIST.REQUEST_FOR_NEW_DATA, (message) => {
+    fs.readFile(helper.MOCK_DATA_URL, function (err, data) {
+      // Check for errors
+      if (err) throw err;
+      // Converting to JSON
+      const source = JSON.parse(data);
+      loadMockData(source);
+
+      //For demo purpose loading data again
+      setTimeout(() => {
+        loadMockData(source);
+      }, 5000);
+      
+    });
+    // io.emit('new-message', message);
+  });
+
+  socket.on(helper.EVENTLIST.DISCONNECT, (reason) => {
+    io.emit(helper.EVENTLIST.CONNECT_STATUS, false);
+    socket.disconnect();
+  });
+});
+
+
+server.listen(port, () => {
+  console.log(`started on port: ${port}`);
+});
+
+function loadMockData(source) {
+  let count = 0;
+  let interval = 1000;
+  let = setInterval(() => {
+    if (count < 5) {
+      let res = source.datatable.data.slice(count, count + 2000);
+      io.emit(helper.EVENTLIST.INCOMING_DATA, res);
+      count++;
+    } else {
+      interval = 0;
+    }
+  }, interval);
+}
+
+
+
+
+
---
+++ constants/common.js
@@ -0,0 +1,18 @@
+// export const DEV_URL = "http://localhost:4200";
+
+// export const MOCK_DATA_URL = "./public/mock_data.json";
+// export const EVENTLIST = {
+//     REQUEST_FOR_NEW_DATA: 'request-for-data',
+//     INCOMING_DATA: 'incoming-data'
+// }
+
+module.exports = Object.freeze({
+    DEV_URL: "http://localhost:4200",
+    MOCK_DATA_URL: "./public/mock_data.json",
+    EVENTLIST: {
+        REQUEST_FOR_NEW_DATA: 'request-for-data',
+        INCOMING_DATA: 'incoming-data',
+        CONNECT_STATUS: 'connect-status',
+        DISCONNECT: 'disconnect_soc'
+    }
+});
